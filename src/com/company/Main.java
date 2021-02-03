b83e3017e4b06d91fad11ecddbd900ff41a626c5
sandesh <qalamsal8@gmail.com>
2021-02-03 15:42:17 -0600

    print master branch updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	// write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Sandesh Lamsal");
-        System.out.println("Branch 1 code");
+        System.out.println("Master Branch !!");
 	//Branch1
 	
     }
