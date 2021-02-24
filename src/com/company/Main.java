7bf2e3d1ad3fc358e97299571fa6788dfb1d7c64
sandesh <qalamsal8@gmail.com>
2021-02-24 12:08:23 -0600

    Removed extra comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
 	   System.out.print("###SANDESH LAMSAL##.");
-        //System.out.println("Master Branch");
-	// write your code here
-        System.out.println("hello world ! from github.com..");
-        System.out.print("Sandesh Lamsal");
-        System.out.println("Master Branch !!");
-	//Branch1
-	
     }
 }
 
