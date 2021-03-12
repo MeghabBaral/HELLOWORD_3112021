46cebe1350b878f07094f9abfaa22a8ca9334dbb
MeghabBaral <69022057+MeghabBaral@users.noreply.github.com>
2021-03-12 14:38:06 -0600

    Update Run.java
    added variables

--- src/Main/Run.java
+++ src/Main/Run.java
@@ -3,6 +3,8 @@ package Main;
 public class Run {
     public static void main(String[] args) {
         Branching master=Branching.Master;
+        int a=10;
+        intb=20;
         System.out.println(master);
     }
 }
